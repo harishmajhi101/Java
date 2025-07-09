288eaf331ff132bc14c5133aeae5676b31c674dc
Harischandra Majhi <harishchandramajhi010@gmail.com>
2025-07-09 14:05:02 +0530

    variables in java

---
+++ Variable.java
@@ -0,0 +1,9 @@
+class Program
+{
+    public static void main(String[]args)
+    {
+        int a=12; //a=12 a is a variable and 12 is a value
+        System.out.println(a);// to print the value of variable use this
+    }
+
+}
